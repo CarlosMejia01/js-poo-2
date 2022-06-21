9a45cb9ea976f48a759f4c16aa48168b9787c5c0
Carlos Mejia <cmejia@equitel.com.co>
2022-06-20 20:45:22 -0500

    types

---
+++ js/types.js
@@ -0,0 +1,16 @@
+//primitive types
+
+console.log(typeof "hello world");
+console.log(typeof 100);
+console.log(typeof false);
+
+
+console.log(typeof {});
+console.log(typeof []);
+
+console.log(typeof new Date());
+console.log(typeof new RegExp());
+
+console.log(typeof new Number(100));
+console.log(typeof new String("hello world"));
+console.log(typeof new Boolean(true));
